456ca254cd8cf7dbc7b7ad11cf713d638ff15d19
Stefan Neben <stefan.neben@gmail.com>
2015-09-28 21:53:39 +0200

    Add exception handling for bottle script

--- src/main/python/afp_ressource_maker/wsgi.py
+++ src/main/python/afp_ressource_maker/wsgi.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import route, default_app
+import traceback
+
+from functools import wraps
+from bottle import route, abort, put, default_app
 
 
-@route('/role/<rolename>')
+def with_exception_handling(old_function):
+    """Decorator function to ensure proper exception handling"""
+    @wraps(old_function)
+    def new_function(*args, **kwargs):
+        try:
+            result = old_function(*args, **kwargs)
+        except Exception:
+            message = traceback.format_exc()
+            abort(500, 'Exception caught {0}'.format(message))
+        return result
+    return new_function
+
+
+@put('/role/<rolename>')
+@with_exception_handling
 def make_role(rolename):
     """Create a role and assign needed policies and trusted entities"""
     pass
