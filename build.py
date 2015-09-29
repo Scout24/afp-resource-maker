9b5baf5d875523192f9c5a9f61a8f79034996e9c
Stefan Neben <stefan.neben@gmail.com>
2015-09-29 14:28:15 +0200

    Install wsgi script into the system

--- build.py
+++ build.py
@@ -1,11 +1,12 @@
 from pybuilder.core import use_plugin, init
 
-use_plugin("python.core")
-use_plugin("python.unittest")
-use_plugin("python.install_dependencies")
-use_plugin("python.flake8")
-use_plugin("python.coverage")
-use_plugin("python.distutils")
+use_plugin('python.core')
+use_plugin('python.unittest')
+use_plugin('python.install_dependencies')
+use_plugin('python.flake8')
+use_plugin('python.coverage')
+use_plugin('python.distutils')
+use_plugin('copy_resources')
 
 
 name = 'afp_ressource_maker'
@@ -25,6 +26,8 @@ def set_properties(project):
     project.depends_on('docopt')
     project.set_property('flake8_include_test_sources', True)
     project.set_property('flake8_break_build', True)
+    project.install_file('/var/www/afp-ressource-maker/', 'wsgi/api.wsgi')
+    project.get_property('copy_resources_glob').extend(['wsgi/*'])
 
 
 @init(environments='teamcity')
